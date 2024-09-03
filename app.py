ebb1ff92ce2882a9a442249a08e58df4c18769cd
Ubuntu <ubuntu@ip-172-31-40-103.us-east-2.compute.internal>
2024-09-03 04:50:33 +0000

    change api from source.com:8000 to api.source.com

--- app.py
+++ app.py
@@ -4,18 +4,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/check_lvl1": {"origins": ["http://dericle.com","http://localhost:5173"]}})
 
-# Define the secret password
 lvl1 = "dle{al4pA_arCaD3_roCK5}"
 
-@app.route('/check_lvl1', methods=['POST'])
+@app.route('/check_lvl1', methods=['GET','POST'])
 def check_lvl1():
-    data = request.get_json()
-    user_input = data.get('password')
+    if request.method == 'POST':
+        data = request.get_json()
+        user_input = data.get('password')
 
-    if user_input == lvl1:
-        return jsonify({"status": "success", "message": "Correct!"}), 200
-    else:
-        return jsonify({"status": "failure", "message": "Incorrect!"}), 400
+        if user_input == lvl1:
+            return jsonify({"status": "success", "message": "Correct!"}), 200
+        else:
+            return jsonify({"status": "error", "message": "Incorrect!"}), 400
+    elif request.method == 'GET':
+        return "nothing to see here!", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
