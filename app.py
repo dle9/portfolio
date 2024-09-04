4081611c21c8efdb5b98d49ae7e9cbfcb0ab3b8d
Ubuntu <ubuntu@ip-172-31-40-103.us-east-2.compute.internal>
2024-09-04 18:10:32 +0000

    add logging to api

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import logging
 import os
+
+logging.basicConfig(
+    filename='api.log',
+    level=logging.DEBUG,
+)
 
 app = Flask(__name__)
 CORS(app, resources={r"/check_lvl1": {"origins": ["https://dericle.com","http://localhost:5173"]}})
@@ -12,11 +18,11 @@ def check_lvl1():
     if request.method == 'POST':
         data = request.get_json()
         user_input = data.get('password')
+        logging.info(user_input)
 
         if user_input == lvl1pass:
             return jsonify({"status": "success", "message": "Correct!"}), 200
         else:
-            print(lvl1pass, user_input)
             return jsonify({"status": "error", "message": "Incorrect!"}), 400
     elif request.method == 'GET':
         return "nothing to see here!", 200
